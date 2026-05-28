1a3f5f108bc95d01824accd48a8d718b08eccd96
DACPRO2 <tapiaruizdante@gmail.com>
2026-05-28 10:16:21 -0600

    segundo commit

---
+++ src/main/java/com/mycompany/poke/Pokemon.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.poke;
+
+
+public class Pokemon {
+    
+   String nombre;
+    int vida;
+    int defensa;
+    int ataque;
+
+    public Pokemon(String nombre, int vida, int defensa, int ataque) {
+        this.nombre = nombre;
+        this.vida = vida;
+        this.defensa = defensa;
+        this.ataque = ataque;
+    }
+    
+}
